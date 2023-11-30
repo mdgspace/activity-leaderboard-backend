08c189eee0b407254618c3fd924015abee106773
yash <yp969803@gmail.com>
2023-11-30 18:09:41 +0530

    feat: setBookmark and archeive status endpoints created

--- src/main/java/com/mdgspace/activityleaderboard/controllers/UserController.java
+++ src/main/java/com/mdgspace/activityleaderboard/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.mdgspace.activityleaderboard.controllers;
 import java.security.Principal;
 import java.util.Optional;
 import java.util.List;
-
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,20 +14,33 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mdgspace.activityleaderboard.models.Organization;
+import com.mdgspace.activityleaderboard.models.Project;
 import com.mdgspace.activityleaderboard.models.User;
+import com.mdgspace.activityleaderboard.models.roles.OrgRole;
+import com.mdgspace.activityleaderboard.payload.request.SetArcheiveStatusRequest;
+import com.mdgspace.activityleaderboard.payload.request.SetBookmarkStatusRequest;
 import com.mdgspace.activityleaderboard.payload.response.MessageResponse;
 import com.mdgspace.activityleaderboard.payload.response.UsersResponse;
+import com.mdgspace.activityleaderboard.repository.OrgRepository;
+import com.mdgspace.activityleaderboard.repository.OrgRoleRepository;
+import com.mdgspace.activityleaderboard.repository.ProjectRepository;
+import com.mdgspace.activityleaderboard.repository.ProjectRoleRepository;
 import com.mdgspace.activityleaderboard.repository.UserRepository;
 import com.mdgspace.activityleaderboard.security.jwt.AuthEntryPointJwt;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-
 
 @RequestMapping("/api/protected/user")
 public class UserController {
@@ -36,34 +49,113 @@ public class UserController {
      @Autowired
      UserRepository userRepository;
 
+     @Autowired
+     ProjectRepository projectRepository;
+
+     @Autowired
+     OrgRepository orgRepository;
+
+     @Autowired
+     OrgRoleRepository orgRoleRepository;
+
+     @Autowired
+     ProjectRoleRepository projectRoleRepository;
+
      @GetMapping("/getUser")
-     public ResponseEntity<?> getLogginedUser(Principal principal){
-        try{
+     public ResponseEntity<?> getLogginedUser(Principal principal) {
+          try {
 
-          String username=principal.getName();
-          Boolean isUser=userRepository.existsByUsername(username);
-          if(!isUser){
-               return ResponseEntity.badRequest().body(new MessageResponse("Invalid token"));
+               String username = principal.getName();
+               Boolean isUser = userRepository.existsByUsername(username);
+               if (!isUser) {
+                    return ResponseEntity.badRequest().body(new MessageResponse("Invalid token"));
+               }
+
+               return ResponseEntity.ok().body(new MessageResponse(username));
+
+          } catch (Exception e) {
+               logger.error("Internal Server Error;", e);
+               return ResponseEntity.internalServerError().body(new MessageResponse("Internal server error"));
           }
-
-          return ResponseEntity.ok().body(new MessageResponse(username));
-        
-        
-        }catch (Exception e){
-          logger.error("Internal Server Error;", e);
-         return ResponseEntity.internalServerError().body(new MessageResponse("Internal server error"));
-        }
      }
 
      @GetMapping("/all")
-     public ResponseEntity<?> getAllUsers(){
-          try{
-               List<User> users=userRepository.findAll();
+     public ResponseEntity<?> getAllUsers() {
+          try {
+               List<User> users = userRepository.findAll();
                return ResponseEntity.ok().body(new UsersResponse(users));
 
-          }catch (Exception e){
+          } catch (Exception e) {
                return ResponseEntity.internalServerError().body(new MessageResponse("Internal Server Error"));
           }
      }
+
+     @PutMapping("/setBookmarkStatus")
+     public ResponseEntity<?> setBookMarkStatus(
+               @Valid @RequestBody SetBookmarkStatusRequest setBookmarkStatusRequest,
+               Principal principal) {
+          try {
+               String username = principal.getName();
+               User user = userRepository.findByUsername(username).orElse(null);
+               Map<String, Boolean> newStatus = setBookmarkStatusRequest.getBookmarkStatus();
+               for (Map.Entry<String, Boolean> e : newStatus.entrySet()) {
+                    String org_name = e.getKey();
+                    Boolean status = e.getValue();
+                    Organization org=orgRepository.findByName(org_name).orElse(null);
+                    if(org==null){
+                         continue;
+                    }
+                    OrgRole orgRole=orgRoleRepository.findByOrganizationAndUser(org, user).orElse(null);
+                    if(orgRole==null){
+                         continue;
+                    }
+                    orgRole.setBookmarked(status);  
+
+               }
+
+               return ResponseEntity.ok().body("Bookmark status changed successfully");
+
+          } catch (Exception e) {
+            logger.error("Internal Server Error", e);
+            return ResponseEntity.internalServerError().body("Internal Server Error");
+
+          }
+     }
+
+
+     @PutMapping("/setArcheiveStatus")
+     public ResponseEntity<?> setArcheiveStatus(
+               @Valid @RequestBody SetArcheiveStatusRequest setArcheiveStatusRequest,
+               Principal principal) {
+          try {
+               String username = principal.getName();
+               User user = userRepository.findByUsername(username).orElse(null);
+               Map<String, Boolean> newStatus = setArcheiveStatusRequest.getArcheiveStatus();
+               for (Map.Entry<String, Boolean> e : newStatus.entrySet()) {
+                    String org_name = e.getKey();
+                    Boolean status = e.getValue();
+                    Organization org=orgRepository.findByName(org_name).orElse(null);
+                    if(org==null){
+                         continue;
+                    }
+                    OrgRole orgRole=orgRoleRepository.findByOrganizationAndUser(org, user).orElse(null);
+                    if(orgRole==null){
+                         continue;
+                    }
+                    orgRole.setArcheive(status);;  
+
+               }
+
+               return ResponseEntity.ok().body("Archeive status changed successfully");
+
+          } catch (Exception e) {
+            logger.error("Internal Server Error", e);
+            return ResponseEntity.internalServerError().body("Internal Server Error");
+
+          }
+     }
+
+     // @GetMapping("/getOrgs/{userName}")
+     // public ResponseEntity<?> getOrgs()
 
 }
--- src/main/java/com/mdgspace/activityleaderboard/repository/OrgRoleRepository.java
+++ src/main/java/com/mdgspace/activityleaderboard/repository/OrgRoleRepository.java
@@ -2,12 +2,18 @@ package com.mdgspace.activityleaderboard.repository;
 
 import java.util.Optional;
 
+import javax.swing.text.html.Option;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mdgspace.activityleaderboard.models.Organization;
 import com.mdgspace.activityleaderboard.models.User;
 import com.mdgspace.activityleaderboard.models.roles.OrgRole;
+import java.util.List;
+
 
 public interface OrgRoleRepository extends JpaRepository<OrgRole,Long>{
     Optional<OrgRole> findByOrganizationAndUser(Organization organization, User user);
+    List<OrgRole> findByUser(User user);
+    
 }
