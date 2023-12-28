ee0b4f6ce3c3c667287d1a70715df34a7466517e
yash <yp969803@gmail.com>
2023-12-28 22:11:36 +0530

    bfix: getProject response bug fixed

--- src/main/java/com/mdgspace/activityleaderboard/models/Project.java
+++ src/main/java/com/mdgspace/activityleaderboard/models/Project.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 
 import java.util.Set;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mdgspace.activityleaderboard.models.roles.ProjectRole;
 
 import jakarta.persistence.*;
@@ -31,6 +31,7 @@ public class Project implements Serializable{
     private String name;
 
 
+    @JsonIgnore
     @NotBlank
     @Size(max=30)
     private String link;
@@ -45,10 +46,12 @@ public class Project implements Serializable{
     @Column(name="archeive")
     private Boolean archeive= false;
 
+    
     @ManyToOne(fetch =FetchType.LAZY, optional = false)
     @JoinColumn(name="organization_id",nullable = false)
     private Organization organization;
-
+    
+    @JsonIgnore
     @OneToMany(mappedBy = "project")
     private Set<ProjectRole> projectRoles=new HashSet<>();
 
